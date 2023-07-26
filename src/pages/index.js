5c22fc676a223b5dc59493b8166bf438eda3c0fb
rhaversen <rhaversen@gmail.com>
2023-07-26 02:04:50 +0100

    setup misc

---
+++ src/pages/index.js
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to My Event Scheduler</h1>
+      <p>Find the best time to meet with your friends!</p>
+    </div>
+  );
+}
+
+export default Home;
