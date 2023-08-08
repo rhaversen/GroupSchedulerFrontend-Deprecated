fd87f19946e99de4b41ab9080c691d3c36d677f5
rhaversen <rhaversen@gmail.com>
2023-08-08 03:19:30 +0200

    DRY

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,67 +1,60 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import axios from 'axios';
 import validator from 'validator';
+
+// Input component to be reused
+const InputField = ({ label, type, name, value, autoComplete, onChange, errorMessage }) => (
+    <div>
+        <label>{label}:</label>
+        <input type={type} name={name} autoComplete={autoComplete} value={value} onChange={onChange} />
+        {errorMessage && <span>{errorMessage}</span>}
+    </div>
+);
 
 function Signup() {
     const [formData, setFormData] = useState({
         username: '',
         email: '',
-        password: ''
+        password: '',
+        confirmPassword: ''
     });
     const [message, setMessage] = useState('');
     const [isLoading, setIsLoading] = useState(false);
     const [isFormValid, setIsFormValid] = useState(false);
+    const [errorMessages, setErrorMessages] = useState({
+        usernameMsg: '',
+        emailMsg: '',
+        passwordMsg: '',
+        confirmPasswordMsg: ''
+    });
 
-    const [usernameMsg, setUsernameMsg] = useState('');
-    const [emailMsg, setEmailMsg] = useState('');
-    const [passwordMsg, setPasswordMsg] = useState('');
-    const [confirmPasswordMsg, setConfirmPasswordMsg] = useState('');
+    const validations = {
+        username: value => (value ? "" : "Please enter a username"),
+        email: value => (validator.isEmail(value) ? "" : "Please enter a valid email."),
+        password: value => (validator.isStrongPassword(value) ? "" : "Password must be strong (e.g., contain letters, numbers, and symbols)."),
+        confirmPassword: value => (value === formData.password ? "" : "Passwords must match.")
+    };
 
     const handleValidation = (name, value) => {
-        if (name === "username" && value === "") {
-            setUsernameMsg("Please enter a username");
-        } else if (name === "username") {
-            setUsernameMsg("");
-        }
-    
-        if (name === "email" && !validator.isEmail(value)) {
-            setEmailMsg("Please enter a valid email.");
-        } else if (name === "email") {
-            setEmailMsg("");
-        }
-    
-        if (name === "password" && !validator.isStrongPassword(value)) {
-            setPasswordMsg("Password must be strong (e.g., contain letters, numbers, and symbols).");
-        } else if (name === "password") {
-            setPasswordMsg("");
-        }
-    
-        if (name === "confirmPassword" && value !== formData.password) {
-            setConfirmPasswordMsg("Passwords must match.");
-        } else if (name === "confirmPassword") {
-            setConfirmPasswordMsg("");
-        }
-    
-        const valid = (
-            validator.isEmail(formData.email) &&
-            validator.isStrongPassword(formData.password) &&
-            formData.username !== ""
-        );
+        const validationMessage = validations[name](value);
+        setErrorMessages({ ...errorMessages, [`${name}Msg`]: validationMessage });
+        
+        const valid = Object.keys(validations).every(key => validations[key](formData[key]) === "");
         setIsFormValid(valid);
     };
+
+    useEffect(() => {
+        // Re-validate the confirmPassword field whenever the password changes, but only if confirmPassword is not empty
+        if (formData.confirmPassword !== "") {
+            handleValidation('confirmPassword', formData.confirmPassword);
+        }
+    }, [formData.password, formData.confirmPassword]);
     
 
     const handleChange = (e) => {
         const { name, value } = e.target;
-        if (formData.hasOwnProperty(name)) {
-            setFormData({ ...formData, [name]: value });
-        }
+        setFormData({ ...formData, [name]: value });
         handleValidation(name, value);
-
-        // Check if passwords match when password is updated
-        if (name === "password" && value !== formData.confirmPassword && formData.confirmPassword) {
-            setConfirmPasswordMsg("Passwords must match.");
-        }
     };
 
     const handleSubmit = async (e) => {
@@ -70,10 +63,8 @@ function Signup() {
 
         try {
             setMessage('');
-            // Assuming your backend endpoint for signup is '/api/v1/users/signup'
             const response = await axios.post(process.env.NEXT_PUBLIC_API_URL + '/api/v1/users', formData);
             setMessage('Signup successful!');
-            // Handle other responses or redirects here
         } catch (error) {
             setMessage(error.response?.data.error || 'Error signing up! Please try again later');
         } finally {
@@ -85,52 +76,12 @@ function Signup() {
         <div>
             <h1>Signup</h1>
             <form onSubmit={handleSubmit}>
-                <div>
-                    <label>Username:</label>
-                    <input
-                        type="text"
-                        name="username"
-                        autoComplete="name"
-                        value={formData.username}
-                        onChange={handleChange}
-                    />
-                    {usernameMsg && <span>{usernameMsg}</span>}
-                </div>
-                <div>
-                    <label>Email:</label>
-                    <input
-                        type="email"
-                        name="email"
-                        autoComplete="email"
-                        value={formData.email}
-                        onChange={handleChange}
-                    />
-                    {emailMsg && <span>{emailMsg}</span>}
-                </div>
-                <div>
-                    <label>Password:</label>
-                    <input
-                        type="password"
-                        name="password"
-                        autoComplete="new-password"
-                        onChange={handleChange}
-                    />
-                    {passwordMsg && <span>{passwordMsg}</span>}
-                </div>
-                <div>
-                    <label>Confirm password:</label>
-                    <input
-                        type="password"
-                        name="confirmPassword"
-                        autoComplete="new-password"
-                        onChange={handleChange}
-                    />
-                    {confirmPasswordMsg && <span>{confirmPasswordMsg}</span>}
-                </div>
-                <button
-                    type="submit"
-                    disabled={!isFormValid || isLoading}>
-                        {isLoading ? 'Signing up...' : 'Signup'}
+                <InputField label="Username" type="text" name="username" autoComplete="name" value={formData.username} onChange={handleChange} errorMessage={errorMessages.usernameMsg} />
+                <InputField label="Email" type="email" name="email" autoComplete="email" value={formData.email} onChange={handleChange} errorMessage={errorMessages.emailMsg} />
+                <InputField label="Password" type="password" name="password" autoComplete="new-password" onChange={handleChange} errorMessage={errorMessages.passwordMsg} />
+                <InputField label="Confirm password" type="password" name="confirmPassword" autoComplete="new-password" onChange={handleChange} errorMessage={errorMessages.confirmPasswordMsg} />
+                <button type="submit" disabled={!isFormValid || isLoading}>
+                    {isLoading ? 'Signing up...' : 'Signup'}
                 </button>
             </form>
             {message && <p>{message}</p>}
