acb7ae33098b230f7d57e6b53d8eaada3c00f65c
rhaversen <rhaversen@gmail.com>
2023-08-21 04:50:39 +0200

    use SEO and crawler friendly

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,43 +1,16 @@
-import { useEffect } from 'react';
-import { useRouter } from 'next/router';
 import Head from 'next/head';
 import axios from 'axios';
+import cookie from 'cookie';
 
 function Index() {
-  const router = useRouter();
-
-  useEffect(() => {
-    const validateToken = async () => {
-      try {
-        const token = localStorage.getItem('jwt'); // Assuming the token is stored in local storage
-
-        if (!token) {
-          router.replace('/promo');
-          return;
-        }
-
-        const response = await axios.post(process.env.NEXT_PUBLIC_API_URL + '/api/v1/validate-token', { token });
-
-        if (response.data.valid) {
-          router.replace('/dashboard'); // Redirect to dashboard if JWT is valid
-        } else {
-          router.replace('/promo'); // Redirect to promo if JWT is not valid
-        }
-      } catch (error) {
-        console.error('Error validating token:', error);
-        router.replace('/promo');
-      }
-    };
-
-    validateToken();
-  }, []);
-
+  // This will be a static page on the client side, so there's no client-side logic required.
+  
   return (
     <div>
       <Head>
         <title>Your Page Title</title>
         <meta name="description" content="A brief description of your page." />
-        <link rel="canonical" href="https://www.yourdomain.com/official-url" /> //TODO
+        <link rel="canonical" href="https://www.yourdomain.com/official-url" />
       </Head>
       {/* You can also add some meaningful content here if needed */}
     </div>
@@ -45,3 +18,49 @@ function Index() {
 }
 
 export default Index;
+
+// Server-side redirection logic
+export async function getServerSideProps(context) {
+  try {
+    // Parse cookies from request headers
+    const parsedCookies = cookie.parse(context.req.headers.cookie || '');
+
+    // Retrieve the token
+    const token = parsedCookies.token;
+    
+    if (!token) {
+      return {
+        redirect: {
+          destination: '/promo',
+          permanent: false,  // This means it's a 302 redirection
+        },
+      };
+    }
+
+    const response = await axios.post(process.env.NEXT_PUBLIC_API_URL + '/api/v1/validate-token', { token });
+
+    if (response.data.valid) {
+      return {
+        redirect: {
+          destination: '/dashboard',
+          permanent: false,
+        },
+      };
+    } else {
+      return {
+        redirect: {
+          destination: '/promo',
+          permanent: false,
+        },
+      };
+    }
+  } catch (error) {
+    console.error('Error validating token:', error);
+    return {
+      redirect: {
+        destination: '/promo',
+        permanent: false,
+      },
+    };
+  }
+}
