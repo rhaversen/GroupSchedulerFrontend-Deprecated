07c5a1828efb9693985cde5f9a864f577ef93ce4
rhaversen <rhaversen@gmail.com>
2023-08-21 19:43:30 +0200

    DRY, readability

---
+++ src/hooks/useUserInputForm.js
@@ -0,0 +1,54 @@
+import { useEffect, useState } from 'react';
+
+const useUserInputForm = (initialValues, validations) => {
+    const [values, setValues] = useState(initialValues);
+    const [errors, setErrors] = useState({});
+    const [isValid, setIsValid] = useState({});
+    const [formIsValid, setFormIsValid] = useState(false);
+    const [isTouched, setIsTouched] = useState({});
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setIsTouched(prev => ({ ...prev, [name]: true }));
+        setValues(prev => ({ ...prev, [name]: value }));
+    };
+
+    useEffect(() => {
+        const validationErrors = { ...errors };
+        const inputIsValid = { ...isValid };
+    
+        for (const key in validations) {
+            if (values[key] !== undefined && isTouched[key]) {
+                const validationResult = validations[key].validate(values[key], values.password);
+                if (validationResult === true) {
+                    inputIsValid[key] = true;
+                    validationErrors[key] = '';
+                } else {
+                    inputIsValid[key] = false;
+                    validationErrors[key] = validationResult;
+                }
+            }
+        }
+    
+        setIsValid(inputIsValid);
+        setErrors(validationErrors);
+    
+        const allFieldsValid = Object.keys(validations).every(key => {
+            return inputIsValid[key];
+        });
+    
+        setFormIsValid(allFieldsValid);
+    }, [values]);
+    
+
+    return {
+        values,
+        errors,
+        isValid,
+        formIsValid,
+        isTouched,
+        handleChange
+    };
+};
+
+export default useUserInputForm;
--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -10,6 +10,7 @@ import * as zxcvbnEnPackage from '@zxcvbn-ts/language-en';
 // Local Modules
 import styles from './userInput.module.scss';
 import InputField from '../components/inputField.jsx';
+import useUserInputForm from '../hooks/useUserInputForm.js';
 
 // Setting up zxcvbn options
 const options = {
@@ -24,57 +25,6 @@ zxcvbnOptions.setOptions(options);
 
 const API_URL = process.env.NEXT_PUBLIC_API_URL + '/api/v1/users' || '';
 
-const useForm = (initialValues, validations) => {
-    const [values, setValues] = useState(initialValues);
-    const [errors, setErrors] = useState({});
-    const [isValid, setIsValid] = useState({});
-    const [formIsValid, setFormIsValid] = useState(false);
-    const [isTouched, setIsTouched] = useState({});
-
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        setIsTouched(prev => ({ ...prev, [name]: true }));
-        setValues(prev => ({ ...prev, [name]: value }));
-    };
-
-    useEffect(() => {
-        const validationErrors = { ...errors };
-        const inputIsValid = { ...isValid };
-    
-        for (const key in validations) {
-            if (values[key] !== undefined && isTouched[key]) {
-                const validationResult = validations[key].validate(values[key], values.password);
-                if (validationResult === true) {
-                    inputIsValid[key] = true;
-                    validationErrors[key] = '';
-                } else {
-                    inputIsValid[key] = false;
-                    validationErrors[key] = validationResult;
-                }
-            }
-        }
-    
-        setIsValid(inputIsValid);
-        setErrors(validationErrors);
-    
-        const allFieldsValid = Object.keys(validations).every(key => {
-            return inputIsValid[key];
-        });
-    
-        setFormIsValid(allFieldsValid);
-    }, [values]);
-    
-
-    return {
-        values,
-        errors,
-        isValid,
-        formIsValid,
-        isTouched,
-        handleChange
-    };
-};
-
 function Register() {
     const validations = {
         username: {
@@ -85,17 +35,15 @@ function Register() {
         },
         password: {
             validate: value => {
-                const result = zxcvbn(value);
                 if (!value) return 'Please enter a password';
-                if (result.score < 2) return `Hackers can crack your password in ${result.crackTimesDisplay.onlineNoThrottling10PerSecond}`;
-                return true;
+                const result = zxcvbn(value);
+                return result.score >= 2 ? true : `Hackers can crack your password in ${result.crackTimesDisplay.onlineNoThrottling10PerSecond}`;
             },
         },        
         confirmPassword: {
             validate: (value, password) => {
                 if (!value) return 'Please confirm your password';
-                if (value !== password) return 'Passwords do not match';
-                return true;
+                return value === password ? true : 'Passwords do not match';
             },
         }
     };
@@ -103,7 +51,7 @@ function Register() {
     const [shouldShake, setShouldShake] = useState(false);
     const [message, setMessage] = useState('');
     const [isLoading, setIsLoading] = useState(false);
-    const { values: formData, errors, formIsValid, isTouched, handleChange } = useForm({
+    const { values: formData, errors, formIsValid, isTouched, handleChange } = useUserInputForm({
         username: '',
         email: '',
         password: '',
@@ -117,7 +65,7 @@ function Register() {
         { type: 'password', name: 'confirmPassword', label: 'Confirm Password', autoComplete: 'new-password' },
     ];
     
-    const shakeButton = () => {
+    const triggerErrorShake = () => {
         setShouldShake(true);
         setTimeout(() => setShouldShake(false), 500);
     }
@@ -131,7 +79,7 @@ function Register() {
             const response = await axios.post(API_URL, formData);
             setMessage(response.data.message);
         } catch (error) {
-            shakeButton();
+            triggerErrorShake();
             setMessage(error.response?.data.error || 'There was a problem with the server signing you up! Please try again later...');
         } finally {
             setIsLoading(false);
