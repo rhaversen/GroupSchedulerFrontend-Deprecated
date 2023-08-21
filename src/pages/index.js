eac95bfd6bfdf651e61e4405b6b66e5e018aba04
rhaversen <rhaversen@gmail.com>
2023-08-21 04:30:17 +0200

    redirecting index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,47 @@
-function Home() {
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import Head from 'next/head';
+import axios from 'axios';
+
+function Index() {
+  const router = useRouter();
+
+  useEffect(() => {
+    const validateToken = async () => {
+      try {
+        const token = localStorage.getItem('jwt'); // Assuming the token is stored in local storage
+
+        if (!token) {
+          router.replace('/promo');
+          return;
+        }
+
+        const response = await axios.post(process.env.NEXT_PUBLIC_API_URL + '/api/v1/validate-token', { token });
+
+        if (response.data.valid) {
+          router.replace('/dashboard'); // Redirect to dashboard if JWT is valid
+        } else {
+          router.replace('/promo'); // Redirect to promo if JWT is not valid
+        }
+      } catch (error) {
+        console.error('Error validating token:', error);
+        router.replace('/promo');
+      }
+    };
+
+    validateToken();
+  }, []);
+
   return (
     <div>
-      <h1>Welcome to My Event Scheduler</h1>
-      <p>Find the best time to meet with your friends!</p>
+      <Head>
+        <title>Your Page Title</title>
+        <meta name="description" content="A brief description of your page." />
+        <link rel="canonical" href="https://www.yourdomain.com/official-url" /> //TODO
+      </Head>
+      {/* You can also add some meaningful content here if needed */}
     </div>
   );
 }
 
-export default Home;
+export default Index;
