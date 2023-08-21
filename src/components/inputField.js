053f7f39a217a0cad21c3fedbb88059ce087361a
rhaversen <rhaversen@gmail.com>
2023-08-21 04:19:57 +0200

    add style, rearrange

--- src/components/inputField.js
+++ src/components/inputField.js
@@ -1,11 +1,18 @@
 import styles from './InputField.module.css';
 
-function InputField({ type, name, label, autoComplete, value, onChange, errorMessage }) {
+function InputField({ type, name, label, autoComplete, value, onChange, errorMessage, color }) {
     return (
         <div className={styles.labelContainer}>
             <label className={styles.label}>{label}</label>
             <div className={styles.inputContainer}>
-                <input className={styles.input} type={type} name={name} autoComplete={autoComplete} value={value} onChange={onChange} />
+                <input
+                    className={styles.input}
+                    type={type} name={name}
+                    autoComplete={autoComplete}
+                    value={value}
+                    onChange={onChange}
+                    style={{ color: color }} 
+                />
                 <div className={styles.errorContainer}>
                     {errorMessage && <span className={styles.error}>{errorMessage}</span>}
                 </div>
