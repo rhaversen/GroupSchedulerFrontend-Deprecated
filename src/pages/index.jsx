47a6c45c7cbdab40fa5ed978a2074b8eab7a3242
rhaversen <rhaversen@gmail.com>
2023-10-19 23:38:45 +0200

    Early SEO

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -6,14 +6,26 @@ function Index () {
   // This will be a static page on the client side, so there's no client-side logic required.
 
   return (
-        <div>
-            <Head>
-                <title>Your Page Title</title>
-                <meta name="description" content="A brief description of your page." />
-                <link rel="canonical" href="https://www.yourdomain.com/official-url" />
-            </Head>
-            {/* You can also add some meaningful content here if needed */}
-        </div>
+    <div>
+      <Head>
+        <title>Rain Date</title>
+        <link
+            rel="canonical"
+            href="https://www.raindate.net"
+        />
+        <meta
+          name="description"
+          content="Your Event Planning App is a one-stop shop for all your event planning needs. With our app, you can view all your upcoming events, create and manage calendars, and see when other users are available. Plus, our event pages let you see more info about events, such as descriptions, dates, and locations."
+        />
+        <meta
+            name="keywords"
+            content="event planning, calendar, events, dashboard, availability, group scheduler, friends, holiday, vacation, plans, rally, reindate"
+        />
+        </Head>
+        {/* Add some meaningful content here if needed, such as a brief overview of your website or a call to action. */}
+        {/* Use internal links to link to other pages on your website. */}
+        {/* Optimize your images for SEO. */}
+    </div>
   )
 }
 
