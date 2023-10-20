0608d5d524810364a5d41d6ef6920c03b7c12e5a
rhaversen <rhaversen@gmail.com>
2023-10-20 18:30:52 +0200

    change to tsx

--- src/pages/new-password.tsx
+++ src/pages/new-password.tsx
@@ -8,34 +8,25 @@ import Link from 'next/link'
 import styles from './userInput.module.scss'
 import InputField from '../components/inputField.jsx'
 import useUserInputForm from '../hooks/useUserInputForm.js'
-import { useUser } from '../contexts/UserContext'
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL + '/api/v1/users/login-local' || ''
+const API_URL = process.env.NEXT_PUBLIC_API_URL + '/api/v1/users/send-reset-password-email' || ''
 
 const validations = {
   email: {
     validate: value => validator.isEmail(value) ? true : <> Please enter a valid email </>
-  },
-  password: {
-    validate: value => value ? true : <> Please enter your password </>
   }
 }
 
 const inputConfigs = [
-  { type: 'email', name: 'email', label: 'Email', autoComplete: 'email' },
-  { type: 'password', name: 'password', label: 'Password', autoComplete: 'current-password' }
+  { type: 'email', name: 'email', label: 'Please enter your email:', autoComplete: 'email' }
 ]
 
-function Login () {
-  const { setUser } = useUser() // Use the user context
-
+function NewPassword () {
   const [message, setMessage] = useState('')
   const [isLoading, setIsLoading] = useState(false)
   const [shouldShake, setShouldShake] = useState(false)
   const { values: formData, errors, formIsValid, handleChange } = useUserInputForm({
-    email: '',
-    password: '',
-    stayLoggedIn: false
+    email: ''
   }, validations)
 
   const triggerErrorShake = () => {
@@ -49,12 +40,11 @@ function Login () {
 
     try {
       setMessage('')
-      const response = await axios.post(API_URL, formData)
-      setMessage(response.data.message)
-      setUser(response.data.user) // Assuming the backend sends user data on successful login.
+      await axios.post(API_URL, formData)
+      setMessage('Please check your email inbox for a password reset link')
     } catch (error) {
       triggerErrorShake()
-      setMessage(error.response?.data.error || 'There was a problem with the server logging you in! Please try again later...')
+      setMessage('There was a problem with the server! Please try again later...')
     } finally {
       setIsLoading(false)
     }
@@ -65,6 +55,7 @@ function Login () {
             <form onSubmit={handleSubmit} className={styles.form}>
                 {inputConfigs.map(input => (
                     <InputField
+                        color="red"
                         key={input.name}
                         type={input.type}
                         name={input.name}
@@ -75,22 +66,15 @@ function Login () {
                         errorMessage={errors[input.name] || ''}
                     />
                 ))}
-                <div className={styles.checkboxContainer}>
-                    <input type="checkbox" name="stayLoggedIn" checked={formData.stayLoggedIn} onChange={handleChange} />
-                    <label htmlFor="stayLoggedIn">Stay logged in</label>
-                </div>
                 <button
                     type="submit"
                     disabled={!formIsValid || isLoading}
                     className={`${styles.submitButton} ${shouldShake ? styles.shake : ''}`}>
-                    {isLoading ? 'Logging in...' : 'Log In'}
+                    {'Request New Password'}
                 </button>
             </form>
             <p className={styles.redirectPrompt}>
                 Don't have an account? <Link href="/signup"><span className={styles.redirectLink}>Sign Up</span></Link>
-            </p>
-            <p className={styles.redirectPrompt}>
-                Forgot your password? <Link href="/new-password"><span className={styles.redirectLink}>Set New Password</span></Link>
             </p>
             {message && <p className={styles.message}>{message}</p>}
         </div>
@@ -103,4 +87,4 @@ export async function getServerSideProps (context) {
   return { props: {} }
 }
 
-export default Login
+export default NewPassword
