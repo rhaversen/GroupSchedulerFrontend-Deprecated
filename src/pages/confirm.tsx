791a85be00b7ade830cc89e1356b6547902aeec1
rhaversen <rhaversen@gmail.com>
2023-12-13 23:31:10 +0100

    Use confirmationCode instead of userCode for confirming new users

--- src/pages/confirm.tsx
+++ src/pages/confirm.tsx
@@ -11,16 +11,16 @@ function Confirm (): JSX.Element {
     const router = useRouter()
 
     useEffect(() => {
-        const userCode = router.query.userCode as string | undefined
-        if (!router.isReady || userCode === '' || userCode == null) return
+        const confirmationCode = router.query.confirmationCode as string | undefined
+        if (!router.isReady || confirmationCode === '' || confirmationCode == null) return
 
-        confirmEmail(userCode)
-    }, [router.isReady, router.query.userCode])
+        confirmEmail(confirmationCode)
+    }, [router.isReady, router.query.confirmationCode])
 
-    const confirmEmail = (userCode: string): void => {
+    const confirmEmail = (confirmationCode: string): void => {
         setMessage('Confirming your email...')
 
-        axios.post(API_V1_URL + 'users/confirm/' + userCode)
+        axios.post(API_V1_URL + 'users/confirm/' + confirmationCode)
             .then(response => {
                 if (response?.data?.message !== '') {
                     setMessage(response.data.message)
