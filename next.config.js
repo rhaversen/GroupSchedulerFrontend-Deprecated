baed67c3184c4a6da68db6b3f34d7c7a935a2a92
rhaversen <rhaversen@gmail.com>
2023-12-14 03:49:12 +0100

    npm install webpack to compile svg

--- next.config.js
+++ next.config.js
@@ -1,8 +1,33 @@
+// Import statement for Next.js configuration typing
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    eslint: {
-        ignoreDuringBuilds: true,
-      }
-}
 
-module.exports = nextConfig
+const nextConfig = {
+  webpack(config) {
+    // First, remove any existing SVG rules
+    config.module.rules = config.module.rules.map((rule) => {
+      if (rule.test?.toString().includes('svg')) {
+        const newRule = { ...rule };
+        newRule.exclude = /\.svg$/i;
+        return newRule;
+      }
+      return rule;
+    });
+
+    // Add the rule to handle SVGs as React components
+    config.module.rules.push({
+      test: /\.svg$/i,
+      use: ['@svgr/webpack'],
+    });
+
+    return config;
+  },
+
+  // ESLint configuration to ignore linting during builds
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+
+  // ...other config
+};
+
+module.exports = nextConfig;
