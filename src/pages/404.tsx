754ce7d4e14109553bee5a56bec2d1318bff258d
rhaversen <rhaversen@gmail.com>
2023-12-23 05:12:05 +0100

    Custom 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import './404.scss'
+import Image from 'next/image'
+import RaindateLogo from '../assets/svg/raindate.svg'
+import Head from 'next/head'
+
+function NotFound() {
+    return (
+        <div className="container">
+            <Head>
+                <title>Raindate - 404</title>
+            </Head>
+
+            <div className="logo">
+                <RaindateLogo/>
+            </div>
+            <div className="black-bar"></div>
+            <Image
+                src="/404/404_ocean.webp"
+                alt="A rough, wide ocean."
+                draggable="false"
+                className="background-image"
+                layout="fill"
+                objectFit="cover"
+                quality={100}
+            />
+            <h1>404</h1>
+            <h2>Sorry, this page got lost at sea</h2>
+            <h3>This app is still under development, please come back later</h3>
+        </div>
+    )
+}
+
+export default NotFound
