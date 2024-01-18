981e8a2355f47a6e294cc7d77c8911dcc6d68276
rhaversen <rhaversen@gmail.com>
2024-01-18 02:47:44 +0100

    Fix root layout, meta tags, titles, favicon and others

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -30,21 +30,6 @@ function Index (): JSX.Element {
 
     return (
         <div>
-            <Head>
-                <title>Rain Date</title>
-                <link rel="canonical" href="https://www.raindate.net"/>
-                <meta
-                    name="description"
-                    content="Your Event Planning App is a one-stop shop for all your event planning needs. With our app, you can view all your upcoming events, create and manage calendars, and see when other users are available. Plus, our event pages let you see more info about events, such as descriptions, dates, and locations."
-                />
-                <meta
-                    name="keywords"
-                    content="event planning, calendar, events, dashboard, availability, group scheduler, friends, holiday, vacation, plans, rally, reindate"
-                />
-            </Head>
-            {/* Add some meaningful content here if needed, such as a brief overview of your website or a call to action. */}
-            {/* Use internal links to link to other pages on your website. */}
-            {/* Optimize your images for SEO. */}
             {/* Conditionally render components based on login status */}
             {isLoggedIn ? <DashboardComponent /> : <LandingComponent />}
         </div>
