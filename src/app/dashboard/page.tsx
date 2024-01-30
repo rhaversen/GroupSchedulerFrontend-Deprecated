f9fc1541e00f9fc5d42029e1201f282115aa7aab
rhaversen <rhaversen@gmail.com>
2024-01-30 02:38:04 +0100

    Dashboard WIP

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -3,6 +3,8 @@
 import React, { useEffect, useState } from 'react'
 
 import fetchUserEvents from '@/utils/fetchUserEvents'
+import CreateCard from '@/components/dashboard/CreateCard'
+import RaindateLogo from '@/components/ui/svg/RaindateLogo'
 
 const Dashboard: React.FC = () => {
     const [userEvents, setUserEvents] = useState<any[]>([])
@@ -23,7 +25,40 @@ const Dashboard: React.FC = () => {
 
     return (
         <div>
-            <h1>User Events</h1>
+            <div className="flex items-center justify-center">
+                <div className="flex items-center space-x-4">
+                    <div className="w-20 h-20">
+                        <RaindateLogo />
+                    </div>
+                    <h1 className="text-6xl text-blue-800">Your Dashboard</h1>
+                </div>
+            </div>
+            <div className="max-w-md mx-auto">
+                <CreateCard 
+                    title={'Create an Event'} 
+                    description={
+                        <>
+                            Let RainDate discover the perfect time for your plans. 
+                            <br/><br/>
+                            From one-on-one dates to large group gatherings, every kind of day can be planned to perfection
+                        </>
+                    }
+                    buttonText={'Make Plans'}
+                    buttonLink={''}
+                    linkText={'Join an Event'}
+                    linkUrl={''}
+                />
+            </div>
+            <div className="max-w-md mx-auto"> {/* Adjust width as needed */}
+                <CreateCard
+                    title={'Create a Group'}
+                    description={'Quickly plan events with your favorite group of people'}
+                    buttonText={'Make Socials'}
+                    buttonLink={''}
+                    linkText={'Join a Group'}
+                    linkUrl={''}
+                />
+            </div>
             {userEvents.length > 0 ? (
                 <ul>
                     {userEvents.map((event, index) => (
