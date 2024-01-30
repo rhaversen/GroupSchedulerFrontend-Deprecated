cebdba3c19f934062f4542f9a6d3098536a25060
rhaversen <rhaversen@gmail.com>
2024-01-30 02:36:55 +0100

    Use tailwind colors, add transition

--- src/components/ui/LinkButton.tsx
+++ src/components/ui/LinkButton.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 const LinkButton = ({ href, buttonText, symbol }: { href: string, buttonText: string, symbol?: React.JSX.Element }) => (
     <p>
         <Link href={href}>
-            <button className="bg-blue-500 text-white flex items-center justify-center px-4 py-2 rounded shadow-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-700 focus:ring-opacity-50">
+            <button className="bg-Highlights text-White flex items-center justify-center px-4 py-2 rounded shadow-md transition-color duration-300 hover:bg-Highlights-hover focus:outline-none focus:ring-2 focus:ring-blue-700 focus:ring-opacity-50">
                 <symbol/>{buttonText}
             </button>
         </Link>
