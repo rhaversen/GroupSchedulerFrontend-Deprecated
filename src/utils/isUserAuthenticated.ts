8732fc7d0eab3064ea6d96e1898ede7208dbc07a
rhaversen <rhaversen@gmail.com>
2024-01-30 23:25:46 +0100

    Better error handling and logging

--- src/utils/isUserAuthenticated.ts
+++ src/utils/isUserAuthenticated.ts
@@ -3,18 +3,41 @@ import Cookies from 'js-cookie'
 
 const API_V1_URL = process.env.NEXT_PUBLIC_API_V1_URL ?? ''
 
-// Function to check user authentication
+/**
+ * Checks if the user is authenticated by verifying the presence and validity of the connect.sid cookie.
+ * @returns {Promise<boolean>} True if the user is authenticated, otherwise false.
+ */
 export default async function checkAuthentication (): Promise<boolean> {
-    const connectSid = Cookies.get('connect.sid');
+    const connectSid = Cookies.get('connect.sid')
+
+    if (process.env.NODE_ENV !== 'production') {
+        console.info('Checking authentication status...')
+    }
 
     if (!connectSid) {
-        return false;
+        console.info('No connect.sid cookie found. User is not authenticated.')
+        return false
     }
 
     try {
-        const res = await axios.get(API_V1_URL + 'users/is-authenticated')
+        const res = await axios.get(`${API_V1_URL}users/is-authenticated`)
+        console.info('Authentication check response status:', res.status)
         return res.status === 200
     } catch (error) {
+        // Convert 'error' to an AxiosError type for better error handling in TypeScript
+        if (axios.isAxiosError(error)) {
+            // The error is an AxiosError
+            if (error.response) {
+                console.error('Server responded with an error:', error.response.status)
+            } else if (error.request) {
+                console.error('No response received for authentication check.')
+            } else {
+                console.error('Error in setting up authentication check request:', error.message)
+            }
+        } else {
+            // The error is not an AxiosError, handling generic or unknown errors
+            console.error('An unexpected error occurred:', error)
+        }
         return false
     }
 }
