358cc9a6b06ce4824eb4443543b42f68d43316ab
rhaversen <rhaversen@gmail.com>
2024-01-31 00:01:56 +0100

    Styling

--- src/components/ui/MessageDisplay.tsx
+++ src/components/ui/MessageDisplay.tsx
@@ -1,7 +1,7 @@
 const MessageDisplay = ({ message }:
 { message: string }) => (
     <div className="flex justify-center">
-        <p className="text-center text-base font-medium text-primary border-2 border-primary rounded p-2 mt-2 max-w-xs bg-background-light overflow-hidden">
+        <p className="text-center text-base font-medium text-primary border-2 border-Highlights rounded p-2 mt-10 max-w-xs bg-background-light overflow-hidden">
             {message}
         </p>
     </div>
