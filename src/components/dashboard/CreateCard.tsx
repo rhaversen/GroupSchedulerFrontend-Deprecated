b452de72cd9f1911d6c06fc21194c186fd8dd2b0
rhaversen <rhaversen@gmail.com>
2024-01-30 02:37:50 +0100

    Create new blank component, icons svg

---
+++ src/components/dashboard/CreateCard.tsx
@@ -0,0 +1,17 @@
+import LinkButton from '@/components/ui/LinkButton';
+import PlusIcon from '@/components/ui/svg/PlusIcon';
+import LinkText from '@/components/ui/LinkText';
+
+const CreateCard = ({ title, description, buttonText, buttonLink, linkText, linkUrl }:
+    { title: string, description: string | JSX.Element, buttonText: string, buttonLink: string, linkText: string, linkUrl: string }) => (
+    <div className="bg-white shadow-lg rounded-lg p-6">
+        <h3 className="text-3xl font-semibold mb-2 text-Dark flex justify-center">{title}</h3>
+        <div className="text-gray-600 mb-4 text-Black">{description}</div>
+        <div className="flex justify-center">
+            <LinkButton href={buttonLink} buttonText={buttonText} symbol={<PlusIcon/>} />
+        </div>
+        <LinkText href={linkUrl} buttonText={linkText} />
+    </div>
+);
+
+export default CreateCard;
